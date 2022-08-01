7e70e3276b459255c07dd3764794693493a18126
Thiago <thiagonascimentodeveloper@gmail.com>
2022-08-01 16:29:25 -0300

    adicionado resposividade ao projeto

--- src/data/AboutMe.tsx
+++ src/data/AboutMe.tsx
@@ -1,8 +1,8 @@
 export const aboutMe = {
     salutation: "Olá, eu sou o Thiago!",
     description: [
-        "Sou natural de Cascavel/CE e tenho 21 anos. Iniciei meus estudos sobre desenvolvimento web em janeiro de 2022 e me reconheço como entusiasta em novas tecnologias do mercado. Com aprendizado adquirido dia após dia.",
-        "Desenvolvo aplicaçoes web de alto nível e valor com React, Typescript, Javascript dentre outras tecnologias.",
-        "Me considero ágil e consistente em aprender uma nova tecnologia e alta adaptabilidade às mudanças, pronto para superar qualquer desafio! Se eu pudesse me definir em 3 palavras, certamente seriam: otimista, perseverante e esforçado!"
+        "Sou natural de Cascavel/CE e tenho 21 anos. Comecei a estudar desenvolvimento web em outubro de 2021 e me reconheço como entusiasta em novas tecnologias do mercado. Com aprendizado adquirido dia após dia.",
+        "Desenvolvo aplicaçoes web de alto nível com React, Typescript, Javascript dentre outras tecnologias.",
+        "Me considero ágil e consistente em aprender uma nova tecnologia e alta adaptabilidade à mudanças, pronto para superar qualquer desafio! Se eu tivesse que me definir em 3 palavras, certamente seriam: otimista, perseverante e esforçado!"
     ]
 }
